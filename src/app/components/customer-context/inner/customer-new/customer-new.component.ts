1d16000704a75357848ebbde13469d0f36ac40e8
HansaWelgama <himanwelgama@gmail.com>
2024-04-26 20:54:20 +0530

    save user avatar in firebase

--- src/app/components/customer-context/inner/customer-new/customer-new.component.ts
+++ src/app/components/customer-context/inner/customer-new/customer-new.component.ts
@@ -3,6 +3,10 @@ import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AngularFireStorage} from "@angular/fire/compat/storage";
+import {finalize, Observable} from "rxjs";
+import {MatProgressBar} from "@angular/material/progress-bar";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-customer-new',
@@ -12,33 +16,64 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/
     MatFormField,
     MatInput,
     MatButton,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatProgressBar,
+    AsyncPipe
   ],
   templateUrl: './customer-new.component.html',
   styleUrl: './customer-new.component.scss'
 })
 export class CustomerNewComponent {
+
+  constructor(private storage:AngularFireStorage) {
+  }
+
+  loading:boolean=false;
+  selectedAvatar:any;
+  // @ts-ignore
+  uploadRate:Observable<number | undefined>;
+  // @ts-ignore
+  downloadLink:Observable<string | undefined>;
+
+
   form= new FormGroup({
     fullName: new FormControl('',[Validators.required]),
     address: new FormControl('',[Validators.required]),
     salary: new FormControl('',[Validators.required]),
     avatar: new FormControl('',[Validators.required])
   })
-  loading:boolean=false;
-  selectedAvatar:any;
+
   saveCustomer() {
+
+    const path='avatar/'+this.form.value.fullName+'/'+this.selectedAvatar.name;
+    const fileRef = this.storage.ref(path);
+    const task = this.storage.upload(path,this.selectedAvatar)
+
+    this.uploadRate = task.percentageChanges();
+
+    task.snapshotChanges().pipe(
+      finalize(()=>{
+        this.downloadLink=fileRef.getDownloadURL();
+      })
+    ).subscribe();
+
+    task.then(()=>{
+      console.log("saved");
+    }).catch(error=>{
+      console.log(error);
+    })
+
+
     let customer={
       fullName:this.form.value.fullName,
       address:this.form.value.address,
       salary:this.form.value.salary,
       avatar:this.selectedAvatar
     }
-    console.log(customer)
+    console.log(customer);
   }
 
-  protected readonly onchange = onchange;
-
-  onchangeFile(event: any) {
+  onChangeFile(event: any) {
     this.selectedAvatar=event.target.files[0];
   }
 }
