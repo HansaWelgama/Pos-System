c197b876ee322e603cf0312c79a3655038846b5b
HansaWelgama <himanwelgama@gmail.com>
2024-04-26 17:46:09 +0530

    add functions into new user component

--- src/app/components/customer-context/inner/customer-new/customer-new.component.ts
+++ src/app/components/customer-context/inner/customer-new/customer-new.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-customer-new',
@@ -10,11 +11,34 @@ import {MatButton} from "@angular/material/button";
     MatLabel,
     MatFormField,
     MatInput,
-    MatButton
+    MatButton,
+    ReactiveFormsModule
   ],
   templateUrl: './customer-new.component.html',
   styleUrl: './customer-new.component.scss'
 })
 export class CustomerNewComponent {
+  form= new FormGroup({
+    fullName: new FormControl('',[Validators.required]),
+    address: new FormControl('',[Validators.required]),
+    salary: new FormControl('',[Validators.required]),
+    avatar: new FormControl('',[Validators.required])
+  })
+  loading:boolean=false;
+  selectedAvatar:any;
+  saveCustomer() {
+    let customer={
+      fullName:this.form.value.fullName,
+      address:this.form.value.address,
+      salary:this.form.value.salary,
+      avatar:this.selectedAvatar
+    }
+    console.log(customer)
+  }
 
+  protected readonly onchange = onchange;
+
+  onchangeFile(event: any) {
+    this.selectedAvatar=event.target.files[0];
+  }
 }
