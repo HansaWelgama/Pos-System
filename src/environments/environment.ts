3d332103fc52397eec3db030232ab56cae3e1d3b
HansaWelgama <himanwelgama@gmail.com>
2024-04-26 22:55:13 +0530

    remove key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,6 @@
 export const environment = {
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   firebaseConfig: {
-    apiKey: "AIzaSyAqzU1xShfB8QlJABvgWX2mEEXUMr2vmLU",
-    authDomain: "pos-angular-282e7.firebaseapp.com",
-    projectId: "pos-angular-282e7",
-    storageBucket: "pos-angular-282e7.appspot.com",
-    messagingSenderId: "553041831113",
-    appId: "1:553041831113:web:0fd4b274f0295ec323d918",
-    measurementId: "G-KTZ32F8CX3"
+
   }
 };
