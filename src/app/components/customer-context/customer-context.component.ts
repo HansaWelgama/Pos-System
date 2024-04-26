ef907f8beeed59c05f53b409c47dffef8ebc83fe
HansaWelgama <himanwelgama@gmail.com>
2024-04-26 14:30:20 +0530

    create folder structure

---
+++ src/app/components/customer-context/customer-context.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-context',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-context.component.html',
+  styleUrl: './customer-context.component.scss'
+})
+export class CustomerContextComponent {
+
+}
