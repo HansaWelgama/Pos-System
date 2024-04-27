536fa2578d89a4ced7f381a8a52ce25505f7b464
HansaWelgama <himanwelgama@gmail.com>
2024-04-27 15:57:12 +0530

    remove APIkey

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,14 +1,5 @@
 export const environment = {
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-firebaseConfig: {
-    apiKey: "AIzaSyBOVyaywSnJwtfpmC8J37a_HVPLuHcsEvQ",
-    authDomain: "pos-system-ca479.firebaseapp.com",
-    databaseURL: "https://pos-system-ca479-default-rtdb.firebaseio.com",
-    projectId: "pos-system-ca479",
-    storageBucket: "pos-system-ca479.appspot.com",
-    messagingSenderId: "599967828082",
-    appId: "1:599967828082:web:3201382e025198f8961aeb",
-    measurementId: "G-WP61N7KH99"
-  }
+
 };
